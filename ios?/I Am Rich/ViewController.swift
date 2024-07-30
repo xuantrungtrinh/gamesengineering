aa590400051cc6463270e75857ad8aec5a254d1b
Trung <105847960+xuantrungtrinh@users.noreply.github.com>
2024-07-30 23:59:04 +0700

    Add files via upload

---
+++ ios?/I Am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Trung on 30/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
